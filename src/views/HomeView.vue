5f6c156a2cd6818c5a77240c127f9c75974c152c
Muhammadamin <webhacker0100@gmail.com>
2022-03-08 14:50:40 +0500

    Added input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
    <div class="searchBar">
        <img src="@/assets/images/main-logo.png" alt="logo">
-       <!--  -->
+      <div class="searchInput">
+        <input type="text">
+      </div>
    </div>
 </template>
 
