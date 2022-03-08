2ad7f5907fabcb5332c2502174713c087bb3b907
Muhammadamin <webhacker0100@gmail.com>
2022-03-08 17:47:20 +0500

    added shadow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="searchBar">
        <img src="@/assets/images/main-logo.png" alt="logo">
-      <div class="searchInput flex items-center justify-between">
+      <div class="searchInput flex items-center justify-between shadow-2xl">
         <input type="text">
         <a><img class="h-8" src="@/assets/images/search-icon.png" alt=""></a>
       </div>
