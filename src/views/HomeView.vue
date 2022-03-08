8b709780436c09a1efafd7fe722908981fbdea1b
Muhammadamin <webhacker0100@gmail.com>
2022-03-08 15:15:26 +0500

    Change tag name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
        <img src="@/assets/images/main-logo.png" alt="logo">
       <div class="searchInput flex items-center justify-between">
         <input type="text">
-        <button><img class="h-8" src="@/assets/images/search-icon.png" alt=""></button>
+        <a><img class="h-8" src="@/assets/images/search-icon.png" alt=""></a>
       </div>
    </div>
 </template>
