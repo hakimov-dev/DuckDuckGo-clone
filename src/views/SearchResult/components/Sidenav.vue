86336bd100f0d8e17076969f9be8741ef1e2f13b
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 13:28:45 +0500

    Added sidenav main content

---
+++ src/views/SearchResult/components/Sidenav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sidenav px-[3%] bg-[#282828]">
+           <div class="close-icon"><svg class="h-8 w-8 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></div>
+        <div class="links flex flex-col items-center justify-center">
+          <h1>This is sidenav</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'sidenav-item'
+}
+</script>
