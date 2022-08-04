2f86afce20b00a765aabc0bbe4676a06a7763431
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 13:59:58 +0500

    Update sidenav

--- src/views/SearchResult/components/Sidenav.vue
+++ src/views/SearchResult/components/Sidenav.vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="sidenav px-[3%] bg-[#282828]">
-           <div class="close-icon"><svg class="h-8 w-8 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></div>
-        <div class="links flex flex-col items-center justify-center">
-          <h1>This is sidenav</h1>
+    <div class="sidenav fixed -right-0 px-[1%] w-[18%] py-2 bg-[#282828] z-[111] top-0 shadow-2xl h-screen">
+           <div class="close-icon"><svg class="h-11 w-11 float-right text-gray-400 transition-all hover:text-gray-500 cursor-pointer p-1"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></div>
+        <div class="links flex flex-col items-center justify-center w-full mx-auto">
+          <h1 class="py-1 pt-2 text-white text-[18px] cursor-pointer">Search history</h1>
+          <a target="_blank" href="https://hakimov.netlify.app/" class="py-1 text-white text-[18px]">Author</a>
+          <a target="_blank" href="https://github.com/hakimov-dev/DuckDuckGo-clone" class="py-1 text-white text-[18px]">Github</a>
+          
+          <a target="_blank" href="https://github.com/hakimov-dev/DuckDuckGo-clone/discussions/1" class="py-1 text-red-500 text-[18px]">Report bug?</a>
+ 
+          <div class="text-center text-white mt-[80%]">
+          <h1 class="text-red-500 uppercase font-medium">Attention</h1>
+          <p>This app for use desktops! Developing maybe later developing for mobile phones.</p>
+          </div>
         </div>
     </div>
 </template>
--- src/views/SearchResult/index.vue
+++ src/views/SearchResult/index.vue
@@ -1,5 +1,6 @@
 <template>
      <Navigation />
+     <Sidenav />
     <div class="results">
         <h1>Results component</h1>
     </div>
@@ -8,5 +9,6 @@
 <script setup>
 /* eslint-disable */
 import Navigation from './components/NaviGation.vue'
+import Sidenav from './components/Sidenav.vue';
 
 </script>
