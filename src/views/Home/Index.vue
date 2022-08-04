f466fab25f1c8e0ddbb93d9511d8e5124a660b3f
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 12:02:16 +0500

    Added v-model

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -2,7 +2,7 @@
    <div class="searchBar">
        <img src="@/assets/images/main-logo.png" alt="logo">
       <div class="searchInput flex items-center justify-between shadow-2xl">
-        <input type="text">
+        <input v-model="$store.state.searchValue" type="text">
         <a><img class="h-8" src="@/assets/images/search-icon.png" alt=""></a>
       </div>
    </div>
