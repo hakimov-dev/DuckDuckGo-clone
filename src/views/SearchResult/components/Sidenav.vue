747be57124364ecbb1419686161a2b9c034c7a2d
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 14:00:48 +0500

    Added v-if

--- src/views/SearchResult/components/Sidenav.vue
+++ src/views/SearchResult/components/Sidenav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sidenav fixed -right-0 px-[1%] w-[18%] py-2 bg-[#282828] z-[111] top-0 shadow-2xl h-screen">
+    <div v-if="$store.state.sidenavOpen" class="sidenav fixed -right-0 px-[1%] w-[18%] py-2 bg-[#282828] z-[111] top-0 shadow-2xl h-screen">
            <div class="close-icon"><svg class="h-11 w-11 float-right text-gray-400 transition-all hover:text-gray-500 cursor-pointer p-1"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></div>
         <div class="links flex flex-col items-center justify-center w-full mx-auto">
           <h1 class="py-1 pt-2 text-white text-[18px] cursor-pointer">Search history</h1>
