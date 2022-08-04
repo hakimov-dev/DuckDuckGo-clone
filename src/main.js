06b1d151bcc19c68001cbee182af0352ad5a843c
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 11:47:35 +0500

    Added vuex

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import '@/assets/styles/main.css'
 import '@/assets/styles/style.css'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
