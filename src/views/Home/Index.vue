742f3105190d3dceb08f30c8e21484dffd8aefe6
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 12:06:41 +0500

    Added click event and update

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -2,8 +2,8 @@
    <div class="searchBar">
        <img src="@/assets/images/main-logo.png" alt="logo">
       <div class="searchInput flex items-center justify-between shadow-2xl">
-        <input v-model="$store.state.searchValue" @keyup.enter="$store.dispatch('keyup.enter', $store.state.searchValue)" type="text">
-        <a><img class="h-8" src="@/assets/images/search-icon.png" alt=""></a>
+        <input v-model="$store.state.searchValue" @keyup.enter="$store.dispatch('postSearchValue', $store.state.searchValue)" type="text">
+        <a @click.prevent="$store.dispatch('postSearchValue', $store.state.searchValue)"><img class="h-8" src="@/assets/images/search-icon.png" alt=""></a>
       </div>
    </div>
 </template>
