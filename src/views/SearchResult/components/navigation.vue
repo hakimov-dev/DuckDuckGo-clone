cba2957b5cbca7f4cc2ee2ed72cabf2eb3dd0162
Muhammadamin <webhacker0100@gmail.com>
2022-08-04 12:44:55 +0500

    Download images

--- src/views/SearchResult/components/navigation.vue
+++ src/views/SearchResult/components/navigation.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="navbar">
-        <h1>This is navigation!</h1>
+    <div class="navbar h-[110px] px-[10%] flex items-center justify-between bg-[#161616]">
+    <img class="w-[120px]" src="@/assets/images/search-logo.png" alt="" srcset="">
+    <input class="bg-[]" v-model="$store.state.searchValue" @keyup.enter="$store.dispatch('postSearchValue', $store.state.searchValue)" type="text" />
     </div>
 </template>
 
