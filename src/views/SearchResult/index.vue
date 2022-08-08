1b4d63fc841da1bdabe35753d055709c7cfd7bda
Muhammadamin <webhacker0100@gmail.com>
2022-08-08 14:21:00 +0500

    Update cards

--- src/views/SearchResult/index.vue
+++ src/views/SearchResult/index.vue
@@ -1,42 +1,44 @@
 <template>
   <Navigation />
   <Sidenav />
-  <div class="results">
+  <div class="results px-[6%]">
     <div
-      class="cards flex items-center flex-col mt-[4%]"
+      class="cards flex items-center flex-col mt-[4%] w-[60%]"
       v-if="$store.state.searchResult"
     >
-      <div
-        class="card group w-[55%] md:mr-[30%] cursor-pointer xl:mr-[33%] max-h-[180px] p-[20px] overflow-hidden shadow-xl bg-[#282828] rounded-xl"
+      <a
+        :href="item.link"
+        target="_blank"
+        class="card my-2 group w-[100%] cursor-pointer  max-h-[180px] p-[20px] overflow-hidden shadow-xl bg-[#282828] rounded-xl"
         v-for="item in $store.state.searchResult"
       >
         <div class="head-links flex items-center">
-          <img
+          <!-- <img
+            v-if="item.pagemap.cse_thumbnail[0] !== undefined"
             class="rounded-full h-[25px] w-[25px]"
-            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTwyK2husUlO6TF4bsP2Jo-sW0i3BQW1u_AnpkybZpNAWJQntyQ6bfdGek"
+            :src="item.pagemap.cse_thumbnail[0].src"
+            alt=""
+           /> -->
+          <img
+            class="rounded-full h-[18px] w-[18px]"
+            :src="`http://www.google.com/s2/favicons?domain=${item.link}`"
             alt=""
           />
-          <h1 class="text-[16px] text-white opacity-90 ml-[15px]">
-            hi Hello world!
+          <h1 class="text-[16px] text-white opacity-90 ml-[10px]">
+           {{item.displayLink}}
           </h1>
         </div>
         <h1
           class="title text-2xl font-medium text-white opacity-90 ml-[15px] group-hover:underline mt-[5px] -ml-[1px]"
         >
-          Hi how are you bro ? whats up?
+         {{item.title}}
         </h1>
         <p
           class="about-text text-white opacity-80 mt-[10px] max-h-[78px] overflow-hidden"
         >
-          Latest information on Coronavirus Disease 2019 (COVID-19) from the
-          County of Hawaii Civil Defense Agency:
-          https://www.hawaiicounty.gov/coronavirus. Disease 2019 (COVID-19) from
-          the County of Hawaii Civil Defense Agency:
-          https://www.hawaiicounty.gov/coronavirus. Disease 2019 (COVID-19) from
-          the County of Hawaii Civil Defense Agency:
-          https://www.hawaiicounty.gov/coronavirus.
+         {{item.snippet}}
         </p>
-      </div>
+      </a>
     </div>
 
   <!-- For loading -->
