99053dc168b7865f00a1283a08e5563c9d7a17c4
Muhammadamin <webhacker0100@gmail.com>
2022-08-08 15:19:45 +0500

    Added border top

---
+++ src/views/SearchResult/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="bg-[#161616] h-[60px] w-full flex items-center border-t-2">
+        <p class="text-white opacity-90 text-[18px]">Since: 2022</p>
+        <p class="text-white opacity-90 text-[18px]">Developed by: <a class="text-white !opacity-100 text-[18px]" target="_blank" href="https://hakimov.netlify.app/#/">&copy; Hakimov-dev</a></p>
+    </footer>
+</template>
