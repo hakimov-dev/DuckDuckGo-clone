206322741b52fd4cfd4f2b7b6c588f1aedff3915
Muhammadamin <webhacker0100@gmail.com>
2022-08-08 15:28:14 +0500

    Update footer

--- src/views/SearchResult/components/Footer.vue
+++ src/views/SearchResult/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="bg-[#161616] h-[60px] w-full flex items-center border-t-2">
-        <p class="text-white opacity-90 text-[18px]">Since: 2022</p>
-        <p class="text-white opacity-90 text-[18px]">Developed by: <a class="text-white !opacity-100 text-[18px]" target="_blank" href="https://hakimov.netlify.app/#/">&copy; Hakimov-dev</a></p>
+    <footer class="bg-[#161616] h-[60px] w-full flex justify-between px-[6%] mt-[10px] items-center border-t-[1px] border-gray-400">
+        <p class="text-white opacity-90 text-[18px]">All right reserved 2022</p>
+        <p class="text-white opacity-90 text-[18px]">Developed by: <a class="text-white !opacity-100 text-[18px]" target="_blank" href="https://hakimov.netlify.app/#/">&copy;Hakimov-dev</a></p>
     </footer>
 </template>
