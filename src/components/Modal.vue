1dc3db5a152375a78af06007cad87a4c8919e8f2
Muhammadamin <webhacker0100@gmail.com>
2022-08-09 09:29:52 +0500

    Added modal content

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="modal">
-        
+    <div class="modal bg-[#2828286e] h-screen w-full relative">
+        <div class="modal-content flex flex-col item-center w-[30%] bg-[#1c1c1c] rounded-full absolute top-[30%] px-[5%]">
+           <div class="card flex items-center w-full justify-between p-[2%]">
+             <img src="../assets/images/icon.png" alt="">
+             <h1>Search item</h1>
+             <p>Search time</p>
+           </div>
+        </div>
     </div>
 </template>
 
