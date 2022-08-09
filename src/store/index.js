40d8f5953c7f15072dfc09efcc9aed9a27f049c7
Muhammadamin <webhacker0100@gmail.com>
2022-08-09 11:32:46 +0500

    Created new clearVariables mutation

--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,10 @@ export default createStore({
       state.userSearchHistory.filter(user => user.searchValue == value)
       state.userSearchHistory.push({searchValue: value, date: `${date.getFullYear()}/${(date.getMonth() > 9 ? date.getMonth() +1 : `0${date.getMonth() +1}`)}/${(date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`)} ${(date.getHours() > 9 ? date.getHours() : '0'+ date.getHours())}:${(date.getMinutes() > 9 ? date.getMinutes() : '0'+ date.getMinutes())}`})
       cookies.set('user_search_history', JSON.stringify(state.userSearchHistory))
+    },
+
+    clearVariables(state){
+      
     }
   },
   actions: {
