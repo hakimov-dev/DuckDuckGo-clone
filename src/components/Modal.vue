1f3945b8a4bdce5b64f72ceaa2f1852b23692972
Muhammadamin <webhacker0100@gmail.com>
2022-08-09 10:12:12 +0500

    Update all

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,6 +2,9 @@
  <transition>
     <div v-if="$store.state.modalOpen" @click="$store.state.modalOpen = false" class="modal bg-[#2828286e] h-screen w-full fixed top-0 z-[111111]">
         <div @click.stop class="modal-content !overflow-y-scroll overflow-x-hidden  h-[400px] flex flex-col item-center w-[50%] bg-[#1c1c1c] rounded-xl absolute top-[25%] left-[25%] p-[2%]">
+          <div class="flex justify-between pb-2 -mt-2">
+           <button class="bg-red-600 py-[6px] px-[18px] rounded-2xl font-medium text-[#e4e4e4] hover:opacity-80 transition-all">Clear all</button>
+          </div>
            <div v-for="item in 30" class="card w-[80%] h-[40px] flex items-center w-full justify-between p-[1%] px-[2%] my-2 text-white shadow shadow-gray-500 rounded-[3px]">
              <img class="h-[20px] w-[20px]" src="../assets/images/icon.png" alt="">
              <h1>Search item</h1>
@@ -25,6 +28,7 @@
 
 .modal-content::-webkit-scrollbar-thumb {
   background:#4b4b4b;
+  color: #e4e4e4;
   border-radius: 3px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -20,9 +20,9 @@ export default createStore({
     postSearchValue (ctx, value) {
       if (value.length > 0) {
         if(navigator.onLine){
+          router.push({ name: 'search', params: { value: value } })
           axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.VUE_APP_API_KEY}&cx=${process.env.VUE_APP_CONTEXT_KEY}&q=${value}`).then(res =>{
           ctx.commit('setResult', res.data.items)
-          router.push({ name: 'search', params: { value: value } })
           }).catch(error => {
             if(error.response.status == 429 || error.response.status == 429)
               alert('Pleace try again later we have too many request...')
