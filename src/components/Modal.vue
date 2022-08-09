2a637bb8965a3f1f790419752844da410e9bb6e1
Muhammadamin <webhacker0100@gmail.com>
2022-08-09 09:54:30 +0500

    Added v-if and click event

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
  <transition>
-    <div class="modal bg-[#2828286e] h-screen w-full fixed top-0 z-[111111]">
-        <div class="modal-content !overflow-y-scroll overflow-x-hidden  h-[400px] flex flex-col item-center w-[50%] bg-[#1c1c1c] rounded-xl absolute top-[25%] left-[25%] p-[2%]">
+    <div v-if="$store.state.modalOpen" @click="$store.state.modalOpen = false" class="modal bg-[#2828286e] h-screen w-full fixed top-0 z-[111111]">
+        <div @click.stop class="modal-content !overflow-y-scroll overflow-x-hidden  h-[400px] flex flex-col item-center w-[50%] bg-[#1c1c1c] rounded-xl absolute top-[25%] left-[25%] p-[2%]">
            <div v-for="item in 30" class="card w-[80%] h-[40px] flex items-center w-full justify-between p-[1%] px-[2%] my-2 text-white shadow shadow-gray-500 rounded-[3px]">
              <img class="h-[20px] w-[20px]" src="../assets/images/icon.png" alt="">
              <h1>Search item</h1>
